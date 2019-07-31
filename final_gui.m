8910ace0decc1184111684ee79b4875caa53b564
rosstutorco <usingerr@wit.edu>
2019-07-31 11:48:04 -0400

    new gui

--- final_gui.m
+++ final_gui.m
@@ -1,42 +1,137 @@
-clc;	% Clear command window.
-clear;	% Delete all variables.
-close all;
+function varargout = final_gui(varargin)
+% FINAL_GUI MATLAB code for final_gui.fig
+%      FINAL_GUI, by itself, creates a new FINAL_GUI or raises the existing
+%      singleton*.
+%
+%      H = FINAL_GUI returns the handle to a new FINAL_GUI or the handle to
+%      the existing singleton*.
+%
+%      FINAL_GUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in FINAL_GUI.M with the given input arguments.
+%
+%      FINAL_GUI('Property','Value',...) creates a new FINAL_GUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before final_gui_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to final_gui_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
 
-message = sprintf('This application attempts to identify the ripeness of strawberries, tomatoes, and bananas. Continue?');
-dialogue_reply = questdlg(message, 'Run program?', 'OK','Cancel', 'OK');
-if strcmpi(dialogue_reply, 'Cancel')
-	% User wishes to cancel the program so exit.
-	return;
-else
-    % User wishes to continue.
-    message2 = sprintf('Do you want to use our demo image or your own image?');
-    dialogue2_reply = questdlg(message2, 'Image selection', 'Demo image', 'My image', 'OK');
-    current_folder = strcat(pwd,'\img');
-    if strcmpi(dialogue2_reply, 'Demo image')
-        % User wants to use a demo image
-        message3 = ('Which demo image do you want to use?');
-        which_image = questdlg(message3, 'Choose an image', 'Strawberry', 'Tomato', 'Banana', 'OK');
-        if strcmpi(which_image, 'Strawberry')
-            base_image = 'strawberry.jpg';
-        elseif strcmpi(which_image, 'Tomato')
-            base_image = 'close_tomato.jpg';
-        elseif strcmpi(which_image, 'Banana')
-            base_image = 'banana.jpg';
-        end
-        image = fullfile(current_folder, base_image);
-    else
-        % User is going to use their own image
-        [file, folder] = uigetfile('*.*', 'Select your image');
-        image = fullfile(folder, file);
-    end
-    
-    % Does the image exist?
-    if ~exist(image, 'file')
-        message4 = 'The selected image does not exist';
-        uiwait(msgbox(message4));
-        return;
-    end
-    
-    imshow(image);
+% Edit the above text to modify the response to help final_gui
+
+% Last Modified by GUIDE v2.5 30-Jul-2019 19:23:23
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @final_gui_OpeningFcn, ...
+                   'gui_OutputFcn',  @final_gui_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
 end
 
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before final_gui is made visible.
+function final_gui_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to final_gui (see VARARGIN)
+
+% Choose default command line output for final_gui
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes final_gui wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = final_gui_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in strawberrydemo.
+function strawberrydemo_Callback(hObject, eventdata, handles)
+% hObject    handle to strawberrydemo (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+image = imread('img/strawberry.jpg');
+axes(handles.axes1);
+imshow(image);
+
+
+% --- Executes on button press in tomatodemo.
+function tomatodemo_Callback(hObject, eventdata, handles)
+% hObject    handle to tomatodemo (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+image = imread('img/tomato.jpg');
+axes(handles.axes1);
+imshow(image);
+
+
+% --- Executes on button press in bananademo.
+function bananademo_Callback(hObject, eventdata, handles)
+% hObject    handle to bananademo (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+image = imread('img/banana.jpg');
+axes(handles.axes1);
+imshow(image);
+
+
+% --- Executes on button press in ownimage.
+function ownimage_Callback(hObject, eventdata, handles)
+% hObject    handle to ownimage (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+[file, folder] = uigetfile();
+fullfile = strcat(folder, file);
+if ~exist(fullfile, 'file')
+    set(handles.text3,'string','This file does not exist');
+else
+    image = imread(fullfile);
+end
+axes(handles.axes1);
+imshow(image);
+
+
+% --- Executes on button press in runbutton.
+function runbutton_Callback(hObject, eventdata, handles)
+% hObject    handle to runbutton (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+%this is where morgan's stuff will go
+
+
+% --- Executes on button press in exitbutton.
+function exitbutton_Callback(hObject, eventdata, handles)
+% hObject    handle to exitbutton (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+close all force;
+return;
